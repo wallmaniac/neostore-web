e051d80bd7b8dcae2999210cf2b31c4ad9a1b6d0
wallmaniac <zzidar1111@gmail.com>
2026-02-22 21:46:05 +0100

    Delete all files

--- functions/contact.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest() {
-  return new Response(JSON.stringify({ success: true, message: 'Poruka je poslana' }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  });
-}
